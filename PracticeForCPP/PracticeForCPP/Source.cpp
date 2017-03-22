3178fc305ed2edff50c0a9bc578785b52a693704
Zoran <zwyliufeng@Outlook.com>
2017-03-22 14:57:26 +0800

    1457

--- PracticeForCPP/PracticeForCPP/Source.cpp
+++ PracticeForCPP/PracticeForCPP/Source.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 int main()
 {
-	std::cout << "Hello,World!"<<std::endl;
+	std::string list1[50], list2[50], list3[50];
+	for (int i = 0; !list1->empty()||!list2->empty(); i++){
+		int flag = 0;
+		for (int j = 0; !list2->empty(); j++)
+			flag = list1[i] == list2[j] ? 1 : 0;
+		list3[i] = flag == 0 ? list1[i] : NULL;	}
 }
